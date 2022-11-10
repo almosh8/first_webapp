12faaee4173e2b1f9a3d0531956bdc7f61453c86
almosh443 <mr.almosh443@mail.ru>
2022-11-10 09:46:01 +0100

    set default values

--- polls/models/items/Item.py
+++ polls/models/items/Item.py
@@ -3,8 +3,8 @@ from django.utils.timezone import now
 
 
 class Item(models.Model):
-    id = models.CharField(max_length=99, primary_key=True, default=None)
-    name = models.CharField(max_length=200, default=None)
+    id = models.CharField(max_length=99, primary_key=True)
+    name = models.CharField(max_length=200, default='unnamed')
     update_date = models.DateTimeField(default=now)
 
     class Meta:
